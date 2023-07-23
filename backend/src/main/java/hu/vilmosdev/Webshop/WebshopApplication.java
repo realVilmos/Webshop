f535983f20bd16098414ab8e06230f8ad9865675
realVilmos <realvilmos@gmail.com>
2023-07-23 14:01:28 +0200

    Cors config, több hibaüzenet kezelése és küldése

--- backend/src/main/java/hu/vilmosdev/Webshop/WebshopApplication.java
+++ backend/src/main/java/hu/vilmosdev/Webshop/WebshopApplication.java
@@ -16,17 +16,4 @@ public class WebshopApplication {
 		SpringApplication.run(WebshopApplication.class, args);
 	}
 
-  @Bean
-  public CorsFilter corsFilter() {
-    CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.addAllowedOrigin("*"); // Allow requests from any origin
-    corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-    corsConfiguration.addAllowedHeader("*"); // Allow all headers
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", corsConfiguration);
-
-    return new CorsFilter(source);
-  }
-
 }
--- backend/src/main/java/hu/vilmosdev/Webshop/config/SecurityConfiguration.java
+++ backend/src/main/java/hu/vilmosdev/Webshop/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package hu.vilmosdev.Webshop.config;
 import hu.vilmosdev.Webshop.user.Role;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +13,16 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +38,7 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
+      .cors(withDefaults())
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests((authorizeHttpRequests) ->
         authorizeHttpRequests
@@ -38,7 +50,16 @@ public class SecurityConfiguration {
 
           .anyRequest()
           .authenticated()
-      )
+      ).exceptionHandling(exceptionHandlingConfigurer -> {
+        exceptionHandlingConfigurer.authenticationEntryPoint((request, response, authException) -> {
+          response.setStatus(HttpStatus.UNAUTHORIZED.value());
+          response.getWriter().write("User not authenticated");
+        });
+        exceptionHandlingConfigurer.accessDeniedHandler((request, response, authException) -> {
+          response.setStatus(HttpStatus.UNAUTHORIZED.value());
+          response.getWriter().write("You do not have permission to access this resource");
+        });
+      })
       .sessionManagement((sessionManagement) ->
         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authenticationProvider(authenticationProvider)
@@ -48,5 +69,21 @@ public class SecurityConfiguration {
           .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
       });
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+    corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+      "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+      "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+    corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+      "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return source;
   }
 }
